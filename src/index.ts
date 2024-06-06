920041e16fb08cfa25a7ee9c783c79f4c44e7438
janat08 <jey.and.key@gmail.com>
2024-06-06 11:50:24 +0500

    add typing

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import { Layout } from './layoutBeercss';
 
 const app = new Hono()
 app.get("*", jsxRenderer(Layout));
-
+declare module "hono" {
+  type ContextRenderer = (
+    content: string | Promise<string>,
+    properties: { title: string }
+  ) => Response;
+}
 app.get('/', (c) => {
   return c.render('Hello Hono!', {title: 'abc'})
 })
